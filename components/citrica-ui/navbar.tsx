795eb45d87830f02594421492bc9b3f1f84768a2
Juan Carlos <juancarlosvigar@gmail.com>
2023-09-18 11:56:25 -0500

    modificacndo navbar

--- components/citrica-ui/navbar.tsx
+++ components/citrica-ui/navbar.tsx
@@ -16,62 +16,27 @@ export default function NavbarCUI() {
     "My Settings",
     "Team Settings",
     "Help & Feedback",
-    "Log Out",
+    
   ];
 
   return (
-    <Navbar
+    <Navbar className="backdrop-blur-none bg-transparent backdrop-saturate-0 border-none"
       isBordered
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarContent className="" justify="start">
+      <NavbarContent className="" justify="end">
         <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-          
-          <p className="font-bold text-inherit">ACwME</p>
-        </NavbarBrand>
-      </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
+      <NavbarMenu className="">
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
               className="w-full"
               color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "primary" : "primary"
               }
               href="#"
               size="lg"
