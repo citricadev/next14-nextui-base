65d9fe5c1f6fd0946cf6bb2af9ca81d7b599dbde
Fernando Morales <fernando.zmorales@gmail.com>
2023-09-20 15:02:27 -0500

    Timeline

--- components/timeline-item.tsx
+++ components/timeline-item.tsx
@@ -2,7 +2,7 @@ const TimelineItem = () => {
   return (
     <>
       <div className="relative center">
-        <div className="timeline-description timeline-description-bl w-full copy copy-bold">
+        <div className="timeline-description timeline-description-bl copy copy-bold">
           <div>Inició la venta mayorista</div>
           <div>en menudencia <span className="text-gold-brand">BLANQUIROJA</span></div>
         </div>
@@ -17,17 +17,20 @@ const TimelineItem = () => {
             1997
           </h4>
         </div>
-        <div className="timeline-description timeline-description-tr w-full copy copy-bold">
+        <div className="timeline-description timeline-description-tr copy copy-bold">
           <div>Fundación de <span className="text-gold-brand">CORPORACIÓN JORDI</span></div>
         </div>
-        <div className="timeline-title timeline-title-right timeline-title-size-3 w-full text-gold-dark">
-          Corporación Jordi
+        <div className="timeline-title timeline-title-right timeline-title-size-3 text-gold-dark top-28 animate-pulse">
+          CORPORACIÓN JORDI
         </div>
       </div>
       <div className="relative center">
-        <div className="timeline-description timeline-description-tl w-full copy copy-bold">
-          <div>Inició la venta mayorista</div>
-          <div>en menudencia <span className="text-gold-brand">BLANQUIROJA</span></div>
+        <div className="timeline-title timeline-title-left timeline-title-size-4 text-gold-dark top-10 animate-pulse animate-duration-[3400ms]">
+          BLANQUIROJA
+        </div>
+        <div className="timeline-description timeline-description-bl copy copy-bold">
+          <div>Inicio de operaciones de </div>
+          <div>importación de menudencias</div>
         </div>
         <picture className="relative">
           <img src={'/img/timeline-item.svg'} alt="timeline item" />
@@ -40,11 +43,18 @@ const TimelineItem = () => {
             2000
           </h4>
         </div>
+        <div className="timeline-description timeline-description-tr2 copy copy-bold">
+          <div>Apertura de control</div>
+          <div>de calidad en el extranjero</div>
+        </div>
       </div>
       <div className="relative center">
-        <div className="timeline-description timeline-description-mr w-full copy copy-bold">
-          <div>Inició la venta mayorista</div>
-          <div>en menudencia <span className="text-gold-brand">BLANQUIROJA</span></div>
+        <div className="timeline-title timeline-title-left timeline-title-size-3 text-gold-dark bottom-4 animate-pulse animate-duration-[2400ms]">
+          Importaciones
+        </div>
+        <div className="timeline-description timeline-description-mr copy copy-bold">
+          <div>Apertura del primer</div>
+          <div>almacén en Huachipa</div>
         </div>
         <picture className="relative">
           <img src={'/img/timeline-item.svg'} alt="timeline item" />
@@ -55,6 +65,202 @@ const TimelineItem = () => {
           </picture>
           <h4 className="display text-gold-brand">
             2003
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-right timeline-title-size-4 text-gold-dark top-0 animate-pulse animate-pulse-[4100ms]">
+          Almacenes
+        </div>
+        <div className="timeline-description timeline-description-ml copy copy-bold">
+          <div> <span className="text-gold-brand">CORPORACIÓN JORDI</span> Inauguración </div>
+          <div> de área de carnes y porcionados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/hub.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2006
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-left timeline-title-size-4 text-gold-dark top-24">
+          Planta
+        </div>
+        <div className="timeline-description timeline-description-mr copy copy-bold">
+          <div> <span className="text-gold-brand">CORPORACIÓN JORDI</span> Inauguración </div>
+          <div> de área de carnes y porcionados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/shelves.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2010
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-right timeline-title-size-4 text-gold-dark top-24">
+          JDS FOOD
+        </div>
+        <div className="timeline-description timeline-description-ml copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/factory.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2013
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-left timeline-title-size-3 text-gold-dark top-8">
+          Restaurantes
+        </div>
+        <div className="timeline-description timeline-description-tr copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <div className="timeline-description timeline-description-bl copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2015
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-left timeline-title-size-4 text-gold-dark top-28 animate-pulse">
+          HOLLYFOOD
+        </div>
+        <div className="timeline-description timeline-description-tr2 copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2016
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-right timeline-title-size-3 text-gold-dark top-8">
+          JORDI PRIME GRILL
+        </div>
+        <div className="timeline-title timeline-title-left timeline-title-size-3 text-gold-dark bottom-0">
+          Venta virtual
+        </div>
+        <div className="timeline-description timeline-description-tl2 copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <div className="timeline-description timeline-description-br2 copy copy-bold">
+          <div>Inauguración de nuestro almacén</div>
+          <div>descentralizado en Trujillo</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2018
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-description timeline-description-tr copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <div className="timeline-description timeline-description-bl copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2020
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-right timeline-title-size-3 text-gold-dark bottom-0">
+          Boutique de carnes
+        </div>
+        <div className="timeline-description timeline-description-tr2 copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <div className="timeline-description timeline-description-bl copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2021
+          </h4>
+        </div>
+      </div>
+      <div className="relative center">
+        <div className="timeline-title timeline-title-left timeline-title-size-3 text-gold-dark top-14">
+          Sedes
+        </div>
+        <div className="timeline-description timeline-description-mr copy copy-bold">
+          <div>Lanzamiento de planta</div>
+          <div>de valores agregados</div>
+        </div>
+        <picture className="relative">
+          <img src={'/img/timeline-item.svg'} alt="timeline item" />
+        </picture>
+        <div className="absolute top-20 w-full center">
+          <picture>
+            <img src={'/img/icons/apartment.svg'} alt="timeline item" />
+          </picture>
+          <h4 className="display text-gold-brand">
+            2022
           </h4>
         </div>
       </div>
