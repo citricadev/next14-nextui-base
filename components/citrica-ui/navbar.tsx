7a8336362f888654da6e9951d74db3a12e291052
Juan Carlos <juancarlosvigar@gmail.com>
2023-09-20 20:52:49 -0500

    cambiosnavbar

--- components/citrica-ui/navbar.tsx
+++ components/citrica-ui/navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-5 bg-black mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-black-gradient absolute top-20 right-5 bg-black mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
         >
           <ul className=" list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
