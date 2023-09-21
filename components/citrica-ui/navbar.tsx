23980f8548dfae741f64dc75fff71a4baf6b19d5
Juan Carlos <juancarlosvigar@gmail.com>
2023-09-21 12:55:17 -0500

    navbar con componente Link

--- components/citrica-ui/navbar.tsx
+++ components/citrica-ui/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-import {Container, Col} from '@citrica/objects'
+import { Container, Col } from '@citrica/objects'
+import Link from "next/link";
 
 export const navLinks = [
   {
@@ -19,19 +20,19 @@ export const navLinks = [
     id: "our-companies",
     title: "Nuestras Empresas",
   },
-	{
+  {
     id: "our-brands",
     title: "Nuestras Marcas",
   },
-	{
+  {
     id: "our-partners",
     title: "Nuestros Socios Estrategicos",
   },
-	{
+  {
     id: "contact-section",
     title: "Contactanos",
   },
-	{
+  {
     id: "platforms",
     title: "Plataformas",
   },
@@ -40,18 +41,18 @@ export const navLinks = [
 const Navbar = () => {
   const [active, setActive] = useState("about-us");
   const [toggle, setToggle] = useState(false);
-  
+
   const closeSidebar = () => {
-    setToggle(false); 
+    setToggle(false);
   };
-  
+
 
 
   return (
     <nav className="w-full  p-3 bg-transparent fixed flex items-center">
       {/* Logo */}
       {/* <h1 className="text-3xl text-black">Logo</h1> */}
-      
+
       {/* Desktop Navigation */}
       {/* <ul className="list-none sm:flex justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -79,23 +80,23 @@ const Navbar = () => {
 
           {/* Sidebar */}
           <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 bg-black-gradient absolute top-20 right-5 bg-black mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            className={`${!toggle ? "hidden" : "flex"
+              } p-6 bg-black-gradient absolute top-20 right-5 bg-black-brand mx-4 my-2 min-w-[140px] rounded-xl z-50 `}
           >
-            <ul className=" list-none flex justify-end items-start flex-1 flex-col">
+            <ul className="  list-none flex justify-end items-start flex-1 flex-col">
               {navLinks.map((nav, index) => (
                 <li
                   key={nav.id}
-                  className={` text-white font-poppins font-medium cursor-pointer hover:text-blue-800 text-[16px] ${
-                    active === nav.title ? "text-blue-300" : "text-dimWhite"
-                  } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                  className={` text-white font-medium cursor-pointer hover:text-blue-800 text-[16px] ${active === nav.title ? "text-red-600" : "text-white"
+                    } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                   onClick={() => {
                     setActive(nav.title);
-                    closeSidebar(); 
+                    closeSidebar();
                   }}
                 >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
+                  <Link href={`#${nav.id}`} prefetch={true}>
+                    {nav.title}
+                  </Link>
                 </li>
               ))}
             </ul>
